79d1c6105c0f8843caf4860effb0989ecf165253
ragnamarie <ragna.steinboemer@gmail.com>
2023-10-06 16:22:11 +0200

    add functions

--- js-forms/js-forms_handle-form-submit/js/index.js
+++ js-forms/js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,17 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  event.target.elements.firstName.focus();
+
+  const formData = new FormData(event.target);
+
+  const complaints = Object.fromEntries(formData);
+
+  console.log(event.target.elements);
+  console.log(complaints);
+
+  event.target.reset();
+});
